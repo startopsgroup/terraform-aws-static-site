b7c7b08bad19391f421db82252d1f76d20d7a058
James Turner <me@jamesturner.im>
2018-12-24 01:04:40 +0000

    Add IPv6 records

--- route53_record/main.tf
+++ route53_record/main.tf
@@ -1,7 +1,19 @@
-resource "aws_route53_record" "alias" {
+resource "aws_route53_record" "ipv4_alias" {
   zone_id = "${var.hosted_zone_id}"
   name = "${var.domain_name}"
   type = "A"
+
+  alias = {
+    name = "${var.cdn_domain_name}"
+    zone_id = "${var.cdn_hosted_zone_id}"
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "ipv6_alias" {
+  zone_id = "${var.hosted_zone_id}"
+  name = "${var.domain_name}"
+  type = "AAAA"
 
   alias = {
     name = "${var.cdn_domain_name}"
