5fab5f1516ba00bc37001af4cf173e65d071f264
James Turner <me@jamesturner.im>
2019-01-02 18:48:50 +0000

    Remove default provider

--- main.tf
+++ main.tf
@@ -1,8 +1,4 @@
 provider "aws" {
-  region = "${var.region}"
-}
-
-provider "aws" {
   alias = "use1"
   region = "us-east-1"
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,3 @@
-variable "region" {}
 variable "domain_name" {}
 variable "not_found_path" {}
 variable "not_found_response_code" {}
